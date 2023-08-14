6d90a4d1d4d24ba4a38b7ffa603dd2eb08bdb027
Armann88 <armankhachaturyann@gmail.com>
2023-08-14 18:28:14 +0400

    firstcommit

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+//1use
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request) // anvanapoxutyun
+    {
+        $data = $request->validated();
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+        $token = $user->createToken('auth_token')->plainTextToken;  // token generation auto
+        $cookie = cookie('token',$token,60 * 24);   //name|token|1 or jamketov  hishecumy
+
+        return response()->json([
+            'user' => new UserResource($user),
+        ])->withCookie($cookie);
+    }
+public function login(LoginRequest $request)
+{
+    $data = $request->validated();
+    $user = User::where('email',$data['email'])->first(); // $data miji emial ->first() bolor toxy
+    if (!$user || !Hash::check($data['password'],$user->password)) {
+        return response()->json([
+            'messege' => 'email or password is wrong'
+        ],401);
+    }
+    $token = $user->createToken('auth_token')->plainTextToken;
+    $cookie = cookie('token',$token,60 * 24);
+    return response()->json([
+        'user' => new UserResource('user')
+    ])->withCookie($cookie);;
+}
+}
---
+++ backend/app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password; //1
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true; //1true
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            // create request controler clons hraman
+            //1
+            'name' => 'required|string|max:255', //partadir|stringic baci vochinch|tareri qanaky
+            'email' => 'required|email|unique:users,email|max:255', // //partadir|regexp|chkrknvox
+            'password' => [
+              'required',
+              'string',
+              Pasword::min(3)->mixedCase()->numbers()->symbols()->uncompromised(),
+              'confirmed',
+            ]
+        ];
+    }
+}
